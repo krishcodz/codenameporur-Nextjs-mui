434536337fb4a0a73175a79ca3fd04774973ac9a
krishcodz <krishnax711@gmail.com>
2023-07-10 07:08:56 +0530

    Updated Header.js and made it responsive

--- components/Header.js
+++ components/Header.js
@@ -1,252 +1,136 @@
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import InputLabel from "@mui/material/InputLabel";
-import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
-import { LoadingButton } from "@mui/lab";
-import React, { useState, useEffect } from "react";
-import banner from "../public/images/db.jpg";
+import { Button, Divider, Grid, TextField, Typography } from "@mui/material";
+import bannermd from "../public/images/db.jpg";
+import bannerxs from "../public/images/mb.jpg";
 import Image from "next/image";
 
-const Header = () => {
-  const EMPTY_USERDATA = {
-    userName: "",
-    phoneNo: "",
-    email: "",
-  };
-  const [userData, setUserData] = useState(EMPTY_USERDATA);
-
+export default function Header() {
   return (
-    <Grid
-      container
-      item
-      xs={12}
-      sx={{
-        display: "flex",
-        alignContent: "flex-start",
-        marginTop: "4%",
-      }}
-    >
+    <Grid item container sx={{ marginTop: "60px", position: "relative" }}>
       <Grid
         item
-        md={8}
-        xs={12}
-        sx={{ position: "relative", height: { xs: "800px", md: "600px" } }}
-      >
-        <Image fill src={banner} alt="banner image" />
-      </Grid>
-
-      <Grid
-        id="checkme"
-        container
-        item
-        md={4}
         xs={12}
         sx={{
-          backgroundColor: "#460405",
-          backgroundImage: "none",
-          display: "flex",
-          alignContent: "flex-start",
-          justifyContent: "center",
-          paddingBottom: "30px",
+          position: "relative",
+          zIndex: "1",
+          display: { md: "flex", xs: "none" },
         }}
       >
-        <Grid item xs={12}>
-          <Typography
-            component="h1"
-            sx={{
-              textAlign: "center",
-              padding: "10px 0",
-              fontSize: { xs: "28px", md: "32px" },
-              fontWeight: "bold",
-              color: "#ffffff",
-            }}
-          >
-            Enquire Now
-          </Typography>
-        </Grid>
+        <Image
+          src={bannermd}
+          alt="banner"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          position: "relative",
+          zIndex: "1",
+          display: { md: "none", xs: "flex" },
+        }}
+      >
+        <Image
+          src={bannerxs}
+          alt="banner"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </Grid>
+      <Grid
+        container
+        item
+        xs={4}
+        sx={{
+          display: { xs: "none", md: "flex" },
+          position: "absolute",
+          top: "5%",
+          right: "0%",
+          zIndex: "2",
+          paddingLeft: "150px",
+          paddingRight: "20px",
+        }}
+      >
         <Grid
           container
           item
-          xs={11}
-          md={9}
-          style={{
-            backgroundColor: "#ffffff",
-            backgroundImage: "none",
-            borderRadius: "5px",
-            padding: "20px",
-            marginTop: "20px",
+          xs={12}
+          sx={{
+            background: "white",
+            paddingX: "20px",
+            paddingY: "10px",
+            borderRadius: "20px",
           }}
         >
-          <Grid item xs={12}>
-            <InputLabel
-              style={{
-                padding: "10px 0",
-                fontSize: "12px",
-                fontWeight: "bold",
-                color: "#000000",
-              }}
-              htmlFor="outlined-basic1"
-            >
-              Name*
-            </InputLabel>
-          </Grid>
-          <TextField
-            type="text"
-            id="outlined-basic1"
-            value={userData.userName}
-            onChange={(e) => {
-              setUserData({
-                ...userData,
-                userName: e.target.value || "",
-              });
-            }}
-            sx={{
-              width: "100%",
-              borderRadius: "5px",
-              "& .MuiOutlinedInput-root .MuiInputBase-input": {
-                background: "#ffffff",
-                padding: "8px",
-                borderRadius: "5px",
-              },
-              "& .MuiFormHelperText-root": {
-                fontSize: "12px",
-              },
-            }}
-            InputProps={{
-              sx: {
-                height: "40px",
-              },
-            }}
-            variant="outlined"
-          />
-          <Grid item xs={12}>
-            <InputLabel
-              style={{
-                padding: "10px 0",
-                fontSize: "12px",
-                fontWeight: "bold",
-                color: "#000000",
-              }}
-              htmlFor="outlined-basic2"
-            >
-              Phone Number*
-            </InputLabel>
-          </Grid>
-          <TextField
-            type="number"
-            id="outlined-basic2"
-            value={userData.phoneNo}
-            onChange={(e) => {
-              setUserData({
-                ...userData,
-                phoneNo: e.target.value || "",
-              });
-            }}
-            sx={{
-              width: "100%",
-              borderRadius: "5px",
-              "& .MuiOutlinedInput-root .MuiInputBase-input": {
-                background: "#ffffff",
-                padding: "8px",
-                borderRadius: "5px",
-              },
-              "& .MuiFormHelperText-root": {
-                fontSize: "12px",
-              },
-            }}
-            InputProps={{
-              sx: {
-                height: "40px",
-              },
-            }}
-            variant="outlined"
-          />
-          <Grid item xs={12}>
-            <InputLabel
-              style={{
-                padding: "10px 0",
-                fontSize: "12px",
-                fontWeight: "bold",
-                color: "#000000",
-              }}
-              htmlFor="outlined-basic3"
-            >
-              Email*
-            </InputLabel>
-          </Grid>
-          <TextField
-            type="email"
-            id="outlined-basic3"
-            value={userData.email}
-            onChange={(e) => {
-              setUserData({
-                ...userData,
-                email: e.target.value || "",
-              });
-            }}
-            sx={{
-              width: "100%",
-              borderRadius: "5px",
-              "& .MuiOutlinedInput-root .MuiInputBase-input": {
-                background: "#ffffff",
-                padding: "8px",
-                borderRadius: "5px",
-              },
-              "& .MuiFormHelperText-root": {
-                fontSize: "12px",
-              },
-            }}
-            InputProps={{
-              sx: {
-                height: "40px",
-              },
-            }}
-            variant="outlined"
-          />
-          <Grid item xs={12} style={{ paddingTop: "30px" }}>
-            <LoadingButton
+          <Grid
+            item
+            xs={12}
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+          >
+            <Typography
+              xs={12}
               sx={{
-                width: "100%",
-                height: "50px",
+                fontSize: "20px",
+                letterSpacing: "3px",
+                fontWeight: "bolder",
+                color: "#3c3c3c",
+              }}
+            >
+              Free Site Visit
+            </Typography>
+            <Divider xs={12} sx={{ my: 2, width: "40%" }}></Divider>
+          </Grid>
+          <Grid item xs={12} sx={{ marginBottom: "20px" }}>
+            <TextField required label="Full Name" fullWidth />
+          </Grid>
+          <Grid item xs={12} sx={{ marginBottom: "20px" }}>
+            <TextField required label="Phone Number" fullWidth />
+          </Grid>
+          <Grid item xs={12} sx={{ marginBottom: "20px" }}>
+            <TextField required label="Email ID" fullWidth />
+          </Grid>
+          <Grid item xs={12} sx={{ marginBottom: "20px" }}>
+            <Button
+              variant="contained"
+              fullWidth
+              sx={{
                 background: "#FBB70F",
-                textTransform: "capitalize",
-                color: "#000000",
-                fontWeight: "bold",
-                fontSize: "14px",
                 ":hover": {
-                  background: "#F79E19",
+                  background: "#FF9900",
                 },
               }}
-              variant="contained"
             >
               Submit
-            </LoadingButton>
+            </Button>
           </Grid>
           <Grid
             item
-            sx={{ paddingY: "20px", paddingX: "25%" }}
-            href="tel:+918588073636"
+            xs={12}
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
           >
             <Typography
+              xs={12}
               sx={{
-                color: "#000000",
-                textAlign: "center",
-                borderBottom: "2px solid #ccc",
-                fontWeight: "lighter",
+                fontSize: "12px",
+                letterSpacing: "3px",
+                color: "#3c3c3c",
               }}
             >
               For More Details
             </Typography>
+            <Divider xs={12} sx={{ my: 2, width: "70%" }}></Divider>
             <Typography
+              xs={12}
               sx={{
-                color: "#000000",
-                textAlign: "center",
-                paddingTop: "10px",
-                fontSize: "25px",
+                fontSize: "28px",
+                letterSpacing: "3px",
+                fontWeight: "bolder",
+                color: "#3c3c3c",
               }}
+              href="tel:+918588073636"
             >
               +918588073636
             </Typography>
@@ -255,6 +139,4 @@ const Header = () => {
       </Grid>
     </Grid>
   );
-};
-
-export default Header;
+}
